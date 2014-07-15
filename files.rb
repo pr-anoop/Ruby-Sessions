d318158e4ad5bd530de5326fdaf6f42c10903ffb
Anoop P R <anoop.pr@experionglobal.com>
2014-07-15 12:38:48 +0530

    File read modified

--- files.rb
+++ files.rb
@@ -1,11 +1,8 @@
-d = Dir.glob("logs/*.log")
-
 output_file = File.new("all.log", "w")
 
-
-d.each  do |x| 
+Dir.glob("logs/*.log").each  do |x| 
 	File.open(x, "r") { |content|
-		output_file.puts(Time.now.strftime "%A, %dth of %B, %Y \n\n" + content.read + "\n\n----------------------------");
+		output_file.puts(Time.now.strftime "%A, %dth of %B, %Y \n\n" + content.read + "\n\n #{'-'*100}");
 	}
 end
 
--- time.rb
+++ time.rb
@@ -1,13 +1,15 @@
 #Get the current time
+require 'active_support/core_ext/integer/inflections';
+
 t = Time.now
 
 #Converting to a different format using strftime
-t.strftime "Today is %A, %dth of %B, %Y" 
+puts t.strftime "Today is %A, %dth of %B, %Y" 
 
 # Output
 # Today is Tuesday, 15th of July, 2014
 
-puts ----------- Alternative -------------
+puts "----------- Alternative -------------"
 
-t.strftime "Today is %A, #{t.day.ordinalize} of %B, %Y"
+puts t.strftime "Today is %A, #{t.day.ordinalize} of %B, %Y"
 
