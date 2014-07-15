194c4f03f457550e6969ee9475e2e726642eeddb
Anoop P R <anoop.pr@experionglobal.com>
2014-07-15 11:49:21 +0530

    Format conversion alternative

--- time.rb
+++ time.rb
@@ -7,3 +7,7 @@ t.strftime "Today is %A, %dth of %B, %Y"
 # Output
 # Today is Tuesday, 15th of July, 2014
 
+puts ----------- Alternative -------------
+
+t.strftime "Today is %A, #{t.day.ordinalize} of %B, %Y"
+
