1a82f0635d1795648d080e0b88cd050f822940c2
Anoop P R <itsanoop@outlook.com>
2014-07-16 11:14:50 +0530

    Modified  files

--- files.rb
+++ files.rb
@@ -1,8 +1,11 @@
-output_file = File.new("all.log", "w")
+output_file = File.new("all.log", "w+")
 
 Dir.glob("logs/*.log").each  do |x| 
 	File.open(x, "r") { |content|
-		output_file.puts(Time.now.strftime "%A, %dth of %B, %Y \n\n" + content.read + "\n\n #{'-'*100}");
+		output = Time.now.strftime "%A, %dth of %B, %Y %I:%M:%S %p \n\n" + content.read.scan(/\w+0/) {|e| e.capitalize} + "\n\n #{'-'*100}"
+		
+		# output_file.puts(output.gsub(/[0-9]/, 'Z').split.map(&:capitalize).join(" "));
+		output_file.puts(output.gsub(/[0-9]/, 'Z'));
 	}
 end
 
